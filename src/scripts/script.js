c52ceedd3f92547eed53ab77a8c70627ec322bb7
Olena Kulichenko <Kulichenko@axinom.com>
2017-07-03 17:27:04 +0300

    Solution page

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -6,9 +6,14 @@
 /*
  * Custom
  */
+//= parties/slick.min.js
 //= parties/jquery.flexslider-min.js
 
 $(document).ready(function(){
+    var clientsSlide = $('.clients-solution div').length;
+    var slidesToShow = 6;
+    var slidesToScroll = clientsSlide%slidesToShow; 
+    console.log(slidesToScroll);
     $('.flexslider').flexslider({
         animation: "slide",
         prevText: "",
@@ -25,5 +30,32 @@ $(document).ready(function(){
         });
         }
     });
-    
+    if(clientsSlide > 6){
+        $('.clients-solution').removeClass('clients-default').slick({
+            dots: false,
+            infinite: false,
+            speed: 600,
+            infinite: true,
+            slidesToShow: slidesToShow,
+            slidesToScroll: slidesToScroll,
+            prevArrow: '<div class="prev"></div>',
+            nextArrow: '<div class="next"></div>',
+            responsive: [
+                    {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4
+                    }
+                    },
+                    {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                }
+            ]
+        });
+    }
 });
